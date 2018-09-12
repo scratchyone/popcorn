ee3144f6989505e4382fd5852030a0d26dc5407e
scratchywon <scratchywon@gmail.com>
2018-09-12 13:26:33 +0000

    JUST WORK PLEASE

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ let needreset = 0.4;
 var oldtime;
 let cheater = 0;
 //functions to make it easier to read numbers. Thanks stackoverflow.
-String.prototype.commafy = function() {
-  return this.replace(/(^|[^\w.])(\d{4,})/g, function($0, $1, $2) {
+String.prototype.commafy = function () {
+  return this.replace(/(^|[^\w.])(\d{4,})/g, function ($0, $1, $2) {
     return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, "$&,");
   });
 };
-Number.prototype.commafy = function() {
+Number.prototype.commafy = function () {
   return String(this).commafy();
 };
 //Weird thing I added to run base64 encoded javascript
-Mousetrap.bind("r c", function(e) {
+Mousetrap.bind("r c", function (e) {
   ec = prompt("Please enter code!");
   if (ec.charAt(0) == "a") {
     eval(atob(ec.substr(1)));
@@ -20,10 +20,11 @@ Mousetrap.bind("r c", function(e) {
   return false;
 });
 //Cheatmode. Adds some buttons so that you can set bags, bps, run code, etc.
-Mousetrap.bind("m", function(e) {
+Mousetrap.bind("m", function (e) {
   if (app.cheatmode == 1) {
     app.cheatmode = 0;
-  } else {
+  }
+  else {
     app.cheatmode = 1;
   }
 });
@@ -38,8 +39,7 @@ if ("addEventListener" in document) {
   );
 }
 //everything you can buy
-var buyable = [
-  {
+var buyable = [{
     name: "Helpful Friends",
     caption: "Friends love to help, for money"
   },
@@ -82,16 +82,16 @@ var buyable = [
 ];
 //prices
 var prices = [
-  {price: 15, bps: 0.1},
-  {price: 100, bps: 1},
-  {price: 500, bps: 0},
-  {price: 10000, bps: 10},
-  {price: 13000, bps: 0},
-  {price: 15000, bps: 100},
-  {price: 50000, bps: 600},
-  {price: 1000000, bps: 2000},
-  {price: 20000000 , bps: 20000},
-  {price: 100000000, bps: 130000},
+  { price: 15, bps: 0.1 },
+  { price: 100, bps: 1 },
+  { price: 500, bps: 0 },
+  { price: 10000, bps: 10 },
+  { price: 13000, bps: 0 },
+  { price: 15000, bps: 100 },
+  { price: 50000, bps: 600 },
+  { price: 1000000, bps: 2000 },
+  { price: 20000000, bps: 20000 },
+  { price: 100000000, bps: 130000 },
 ];
 //reset purchases array
 var purchased = [];
@@ -102,11 +102,11 @@ for (let i = 0; i < buyable.length; i++) {
 Vue.component("buyable", {
   template: "#buyable-template",
   props: ["purchase", "index"],
-  data: function() {
+  data: function () {
     return this.$root.$data; //import the main instinces variables
   },
   methods: {
-    buy: function(item, index) { //runs when you click buy
+    buy: function (item, index) { //runs when you click buy
       if (this.purchased.length < index) { //if the purchases array is too short, fix it
         while (this.purchased.length < index) {
           purchased.push(0);
@@ -115,21 +115,22 @@ Vue.component("buyable", {
       if (this.bags >= this.prices[index].price) { //check if you can afford it
         let b = 1; //if you can buy it
         if (this.purchased[index] > 0) { //if you already own at least one of the item you want to buy
-          switch (index) { 
-            case 2: //it's an upgrade, you can only buy one
-              noup(); //display error
-              b = 0; //you can't buy it
-              break;
-            case 4: //it's an upgrade, you can only buy one
-              noup(); //display error
-              b = 0; //you can't buy it
-              break;
+          switch (index) {
+          case 2: //it's an upgrade, you can only buy one
+            noup(); //display error
+            b = 0; //you can't buy it
+            break;
+          case 4: //it's an upgrade, you can only buy one
+            noup(); //display error
+            b = 0; //you can't buy it
+            break;
           }
         }
         if (b == 1) { //if you can buy it
           if (up.luck == 1 && Math.floor(Math.random() * 201) == 1) { //if you have the luck upgrade and a random function gets the correct number
             notie.alert({ type: "success", text: "It's free!" }); // you get it for free
-          } else {
+          }
+          else {
             this.bags -= this.prices[index].price; // remove the bags
           }
           this.bps += this.prices[index].bps; //add the purchase's bps to your bps. It's not calculated...
@@ -138,7 +139,8 @@ Vue.component("buyable", {
           this.prices[index].price += this.prices[index].price * 0.05; //make it more expensive
           upgrade(); // check if you bought an upgrade. If so, fix the upgrades object
         }
-      } else { //you can't afford it
+      }
+      else { //you can't afford it
         sp = (this.prices[index].price - app.bags) / app.bps; //detect how long it will take to be able to afforf it
         x = "You will be able to afford it in " + Math.round(sp) + " seconds."; //set output
         if (Math.round(sp) >= 60) { //if it's at least a minute until you can afford it
@@ -183,13 +185,15 @@ Vue.component("buyable", {
       }
       return `blur(${blur}px)`; //return the blur amount
     },
-    color: function(price) { //gradient for button
+    color: function (price) { //gradient for button
       if (price < this.bagdisp) { //if you can afford it, just be completely green
         return { "background-color": "#3BEC10" };
-      } else {
+      }
+      else {
         if (this.bagdisp / price * 100 < 2) { //if it's a really small percentage, just make it blue to reduce lag
           return { "background-color": "#10B6EC" };
-        } else {
+        }
+        else {
           let percent = this.bagdisp / price * 100;
           return {
             "background-image": `linear-gradient(90deg, #3BEC10, #3BEC10 ${percent}%, #10B6EC 0)`, //weird gradent code. I have no clue how it works. ask mdn
@@ -202,31 +206,31 @@ Vue.component("buyable", {
 });
 Vue.component("stats", { //statistics sidebar
   template: "#stats-template",
-  data: function() {
+  data: function () {
     return this.$root.$data; //import the main vue intences variables
   },
   methods: {
-    calc: function(input) { //function to grab all the needed variables and round them, etc
+    calc: function (input) { //function to grab all the needed variables and round them, etc
       switch (input) {
-        case "bought": //if you want to grab how many items you own
-          var x = 0;
-          for (let i = 0; i < this.$root.$data.purchased.length; i++) { //add together all the numbers of purchased items. Probably should be foreach
-            x += this.$root.$data.purchased[i];
-          }
-          return Math.round(x * 10) / 10; //round it
-        case "money": //get totalspent
-          return Math.round(this.$root.$data.totalspent * 10) / 10; //round it
+      case "bought": //if you want to grab how many items you own
+        var x = 0;
+        for (let i = 0; i < this.$root.$data.purchased.length; i++) { //add together all the numbers of purchased items. Probably should be foreach
+          x += this.$root.$data.purchased[i];
+        }
+        return Math.round(x * 10) / 10; //round it
+      case "money": //get totalspent
+        return Math.round(this.$root.$data.totalspent * 10) / 10; //round it
       }
     }
   }
 });
 Vue.component("game", { //main page
   template: "#app-template",
-  data: function() {
+  data: function () {
     return this.$root.$data; //import main vue instances data
   },
   methods: {
-    popsingle: function() { //pop a single bag. what more is there to add?
+    popsingle: function () { //pop a single bag. what more is there to add?
       this.bags += up.click; //how many bags you get per pop
     }
   }
@@ -250,72 +254,98 @@ document.body.onload = init(); //trigger init function on body load
 function upgrade() { //detect upgrades
   if (app.purchased[2] >= 1) { //if you own item 2
     up.click = 2; //make the bags per click 2
-  } else {
+  }
+  else {
     up.click = 1;
   }
   if (app.purchased[4] >= 1) { //if you own item 4
     up.luck = 1; //used in buyable component
-  } else {
+  }
+  else {
     up.luck = 0;
   }
 }
+
 function fix() { //fix version incompatabilities. TAKE THAT COOKIE CLICKER
   switch (version) { //get version of save file
-    case 0.0: //code to move stuff around to fix problems
-      app.prices.splice(2, 0, prices[2]);
-      app.purchased.splice(2, 0, 0);
-      version = 0.1;
-      fix();
-      break;
-    case 0.1: //same as above
-      app.prices.splice(4, 0, prices[4]);
-      app.purchased.splice(4, 0, 0);
-      version = 0.2;
-      break;
-    case 0.2:
+  case 0.0: //code to move stuff around to fix problems
+    app.prices.splice(2, 0, prices[2]);
+    app.purchased.splice(2, 0, 0);
+    version = 0.1;
+    fix();
+    break;
+  case 0.1: //same as above
+    app.prices.splice(4, 0, prices[4]);
+    app.purchased.splice(4, 0, 0);
+    version = 0.2;
+    break;
+  case 0.2:
     console.log("0.2")
-app.prices=prices;
-app,purchased=[];
-for (let i = 0; i < buyable.length; i++) {
-  app.purchased.push(0);
-}
-      break;
-      case 0.3:
-        app.prices=prices
+    app.prices = prices;
+    app, purchased = [];
+    for (let i = 0; i < buyable.length; i++) {
+      app.purchased.push(0);
+    }
+    break;
+  case 0.3:
+    app.prices = [
+      { price: 15, bps: 0.1 },
+      { price: 100, bps: 1 },
+      { price: 500, bps: 0 },
+      { price: 10000, bps: 10 },
+      { price: 13000, bps: 0 },
+      { price: 15000, bps: 100 },
+      { price: 50000, bps: 600 },
+      { price: 1000000, bps: 2000 },
+      { price: 20000000, bps: 20000 },
+      { price: 100000000, bps: 130000 },
+    ]
   }
 }
+
 function dispa() { //set the constantly updating bags counter. ONLY FOR SHOW. Not used for purchases
   if (app.bps > 20) { //if you get more than 20 bps, dont show decimals
     app.bagdisp = Math.round(
       app.bags + (Date.now() - oldtime) / 1000 * (app.bps * app.bpsmult)
-    ); 
+    );
     /*
     Get how long it has been since the last app.bags update in seconds
     Then, multiply that by the bps (multiplied the the bps multiplier in case of a golden popcorn)
     Then, add the app.bags to it
     Basically, guess how far along it should be based on how long it has been since the last app.bags update
     */
-  } else {
+  }
+  else {
     app.bagdisp =
       Math.round(
         (app.bags + (Date.now() - oldtime) / 1000 * (app.bps * app.bpsmult)) *
-          10
+        10
       ) / 10; //same as abouve, but roundes to nearest tenth
   }
   window.requestAnimationFrame(dispa)
 }
 
 function baga() { //code to calculate the REAL bag counter. This one is used for purchases and stuff
-  if (Date.now() - oldtime > 1200) {// if it's been more than 1.2 seconds since the last update, you were probably in another tab. That can stop this from updating.
+  if (Date.now() - oldtime > 1200 && (Date.now() - oldtime) < 10800000) { // if it's been more than 1.2 seconds since the last update, you were probably in another tab. That can stop this from updating.
     app.bags += (Date.now() - oldtime) / 1000 * (app.bps * app.bpsmult); //the same code as in the function for app.bagdisp. Just guess based on the bps and how long it has been since the last update
     oldtime = Date.now() - 1; //set the last updated time. It's set to 1 millesecond ago to prevent possible errors
     console.log("Timing error");
-  } else {
+  }
+  else if (Date.now() - oldtime >= 10800000) {
+    swal( //you were gone for too long, preventing working overnight
+      "You were gone too long!",
+      'You haven\'t opened this tab in 3 hours. After 3 hours, you stop gaining popcorn unless you go back to this tab. This is to prevent you from getting millions of popcorn overnight.',
+      "warning")
+    app.bags += 10800000 / 1000 * (app.bps * app.bpsmult); //the same code as in the function for app.bagdisp. Just guess based on the bps and how long it has been since the last update
+    oldtime = Date.now() - 1; //set the last updated time. It's set to 1 millesecond ago to prevent possible errors
+    console.log("Timing error, gone too long");
+  }
+  else {
     oldtime = Date.now() - 1; //set the last updated time. It's set to 1 millesecond ago to prevent possible errors
     app.bags += app.bps * app.bpsmult; //set app.bags to the bps times the bps multiplier
   }
   window.setTimeout(updatetitle(), 0); //update the title async. This is really bad code but why write good code when you can use settimeout?
-  if (Date.now() - bpsmultreset > 30000) {//if you have had multiplied bps for 30 seconds, shut it off. Bad code again. This should be somewhere else. I'll fix it later.
+  if (Date.now() - bpsmultreset > 30000) { //if you have had multiplied bps for 30 seconds, shut it off. Bad code again. This should be somewhere else. I'll fix it later.
     app.bpsmult = 1;
   }
 }
@@ -334,12 +364,11 @@ function load() { //load savefiles
       document.body.style.backgroundImage = "url('cheaterwhite.png')";
     }
     if (savefile[5] < needreset) { //if the savefile was saved on a previous version
-      let version = savefile[5]; 
+      let version = savefile[5];
       swal( //warn that your save is messed up and ask if you want the game to automatiaclly fix it
         "Don't click away!",
         'Since you last played, there was an update that might break your game. You might own things that you never bought, or not own things that you did buy. Just click "Fix it", and the intelligent repair system should resolve all of the conflicts.',
-        "warning",
-        {
+        "warning", {
           buttons: {
             cancel: "Ok, I'm fine with issues.",
             catch: {
@@ -350,14 +379,14 @@ function load() { //load savefiles
         }
       ).then(value => {
         switch (value) {
-          case "fix":
-            fix(); //if you say yes, fix it
-            swal( //tell the user it's fixed
-              "Fixed",
-              "The issue should be fixed! Your upgrades and purchased should still be correct.",
-              "success"
-            ).then(value => {});
-            break;
+        case "fix":
+          fix(); //if you say yes, fix it
+          swal( //tell the user it's fixed
+            "Fixed",
+            "The issue should be fixed! Your upgrades and purchased should still be correct.",
+            "success"
+          ).then(value => {});
+          break;
         }
       });
     }
@@ -387,6 +416,7 @@ function reset() { //reset everything
   save(); //save
   location.reload(); //reload the page. Fixes everything
 }
+
 function updatetitle() {
   let chosenitem = 0; //next item you can afford
   let title = ""; // title to be set
@@ -416,34 +446,40 @@ function save() { //code to save
   createCookie("pcsave", btoa(JSON.stringify(savee))); //turn it to a string, base64 encode it, and save it
   //console.log("saved!");
 }
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
 function golden() { //golden cookies
   hi = document.getElementById("golden"); //get the golden popcorn element
   hi.style.top = random(100, window.innerHeight - 100) + "px"; //move it to a random height
   hi.style.left = random(100, window.innerWidth - 100) + "px"; //move it to a random width
   hi.className = "show"; //make it show up and animate
-  setTimeout(function() {
+  setTimeout(function () {
     hi.className = "hide"; //hide it
   }, 7000);
 }
+
 function noup() { //tell you that you can't buy more than one of each upgrade
   notie.alert({ type: "info", text: "You can only buy one of each upgrade!" });
 }
+
 function trygolden() { //try to get a golden popcorn. 1 in 500 chance
   if (random(0, 500) == 250) {
     golden();
   }
 }
+
 function goldenclick() { //triggered when you click a golden popcorn
   //document.getElementById("golden").style.visibility = "none"; //hide it so you can't click it again
   document.getElementById("golden").className = "hide"; //give it the "hide" class
-  notie.alert({ type: "success", text: "5x bps for 30 seconds!" });  //tell you what you got
+  notie.alert({ type: "success", text: "5x bps for 30 seconds!" }); //tell you what you got
   app.bpsmult = 5; //set the bps multiplier
   bpsmultreset = Date.now(); //start the timer
 }
 bpsmultreset = Date.now();
+
 function init() { //init function
   load(); //load save
   upgrade(); //detect upgrades
